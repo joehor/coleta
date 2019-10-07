e41e95b997758190387eaa832dfee13b46bc3835
MARCELO HORLLE <joehor@gmail.com>
2019-10-07 14:00:00 -0300

    melhorias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,20 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ProgressbarModule, TooltipModule } from 'ngx-bootstrap';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { ColetaComponent } from './coleta/coleta.component';
 import { LoginComponent } from './login/login.component';
 import { ColetainjetadaComponent } from './coletainjetada/coletainjetada.component';
+import { NavmenuComponent } from './navmenu/navmenu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ColetaComponent,
     LoginComponent,
-    ColetainjetadaComponent
+    ColetainjetadaComponent,
+    NavmenuComponent
   ],
   imports: [
     BrowserModule,
@@ -25,6 +28,7 @@ import { ColetainjetadaComponent } from './coletainjetada/coletainjetada.compone
     BrowserAnimationsModule,
     FormsModule,
     ProgressbarModule.forRoot(),
+    AngularFontAwesomeModule,
     TooltipModule.forRoot()
   ],
   providers: [],
