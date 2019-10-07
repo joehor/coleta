a38786464d9aca23905054608bb3fca17a0bde6e
MARCELO HORLLE <joehor@gmail.com>
2019-10-07 13:58:54 -0300

    upload files

---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+
+  constructor(private httpClient: HttpClient ) { }
+
+  /*
+  postFile(fileToUpload: File): Observable<boolean> {
+    const endpoint = 'your-destination-url';
+    const formData: FormData = new FormData();
+    formData.append('fileKey', fileToUpload, fileToUpload.name);
+    return this.httpClient
+      .post(endpoint, formData, { headers: yourHeadersConfig })
+      .map(() => true)
+      .catch((e) => this.handleError(e));
+  }
+  */
+
+}
