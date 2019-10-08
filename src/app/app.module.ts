0f48e6003118d9de3105bb5d7c615e35892c6aae
MARCELO HORLLE <joehor@gmail.com>
2019-10-08 14:45:30 -0300

    modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ProgressbarModule, TooltipModule } from 'ngx-bootstrap';
+import { ProgressbarModule, TooltipModule, ModalModule } from 'ngx-bootstrap';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { ColetaComponent } from './coleta/coleta.component';
@@ -27,8 +27,9 @@ import { NavmenuComponent } from './navmenu/navmenu.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    ProgressbarModule.forRoot(),
+    ModalModule.forRoot(),
     AngularFontAwesomeModule,
+    ProgressbarModule.forRoot(),
     TooltipModule.forRoot()
   ],
   providers: [],
