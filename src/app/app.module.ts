a12d323ef7ff24e6e2f5498179bde91748ba4570
MARCELO HORLLE <horlle@kappesberg.com.br>
2019-10-13 00:44:02 -0300

    novos servicos e componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule , FormsModule, FormBuilder } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { ProgressbarModule, PaginationModule, TooltipModule, ModalModule } from 'ngx-bootstrap';
+import { ProgressbarModule
+  , PaginationModule
+  , TooltipModule
+  , ModalModule
+  , AlertModule
+  , AlertConfig
+  // , BsDropdownModule
+} from 'ngx-bootstrap';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { ColetaComponent } from './coleta/coleta.component';
@@ -15,8 +25,12 @@ import { ColetainjetadaComponent } from './coletainjetada/coletainjetada.compone
 import { NavmenuComponent } from './navmenu/navmenu.component';
 import { FilterPipe } from './pipes/filter.pipe';
 
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpParams, HttpRequest, HttpHandler, HttpEvent  } from '@angular/common/http';
 import { TokenInterceptor } from './services/auth/token.interceptor';
+import { AuthService } from './services/auth/auth.service';
+import { RepresentantesLookupComponent } from './representantes-lookup/representantes-lookup.component';
+import { GridComponent } from './grid/grid.component';
+
 
 @NgModule({
   declarations: [
@@ -25,13 +39,21 @@ import { TokenInterceptor } from './services/auth/token.interceptor';
     LoginComponent,
     ColetainjetadaComponent,
     NavmenuComponent,
-    FilterPipe
+    FilterPipe,
+    RepresentantesLookupComponent,
+    GridComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     FormsModule,
+    HttpClientModule,
+
+    // BsDropdownModule.forRoot(),
+
+    AlertModule,
     ModalModule.forRoot(),
     AngularFontAwesomeModule,
     ProgressbarModule.forRoot(),
@@ -43,7 +65,11 @@ import { TokenInterceptor } from './services/auth/token.interceptor';
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
       multi: true
-    }
+    },
+    AuthService,
+    FormBuilder,
+    AlertConfig,
+    HttpParams,
   ],
   bootstrap: [AppComponent]
 })
