1a869c25fc10e1996f661f4c5f6937bdf11f2852
MARCELO HORLLE <horlle@kappesberg.com.br>
2019-10-13 00:43:10 -0300

    rota de representantes (remover a rota)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { ColetaComponent } from './coleta/coleta.component';
 import { LoginComponent } from './login/login.component';
 import { ColetainjetadaComponent } from './coletainjetada/coletainjetada.component';
+import { RepresentantesLookupComponent } from './representantes-lookup/representantes-lookup.component';
 
 const routes: Routes = [
   { path: 'coleta', component: ColetaComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'represlookup', component: RepresentantesLookupComponent },
   { path: 'coletainject', component: ColetainjetadaComponent },
 ];
 
