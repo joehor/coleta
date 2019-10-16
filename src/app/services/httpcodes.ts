0c599ea301052587db2765c6c412d4e794b40b67
MARCELO HORLLE <horlle@kappesberg.com.br>
2019-10-16 00:27:57 -0300

    novos campos

---
+++ src/app/services/httpcodes.ts
@@ -0,0 +1,13 @@
+export interface Httpcodes {
+    headers: {
+      normalizedNames: {},
+      lazyUpdate: any
+    };
+    status: number;
+    statusText: string;
+    url: string;
+    ok: boolean;
+    name: string;
+    message: string;
+    error: any;
+}
