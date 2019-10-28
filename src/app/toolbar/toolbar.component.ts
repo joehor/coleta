e7e21e37ddec89ad0ead605f4e82f82536bd4abb
MARCELO HORLLE <horlle@kappesberg.com.br>
2019-10-28 00:32:55 -0300

    ajuste aparencia

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
+import { getLocaleEraNames } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
@@ -10,14 +11,26 @@ export class ToolbarComponent implements OnInit {
   @Output() emitSidemenu = new EventEmitter();
   useractive = false;
   sidmenu = false;
+  notifylist: any[] = [];
 
   constructor( private auth: AuthService ) {
 
-    this.auth.emitisLoggin.subscribe(ok => this.useractive = ok);
+    this.useractive = this.auth.isAuthenticated();
 
   }
 
   ngOnInit() {
+
+
+
+
+    this.notifylist.push({id: 1, assunto: 'teste 1', corpo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis tincidunt leo, nec semper est tempus sit amet. Suspendisse eu ligula eu tellus convallis auctor. Aenean ac pellentesque tellus, at vehicula odio. Pellentesque euismod lacinia libero vitae vehicula. Sed nec ullamcorper quam. Curabitur aliquam interdum posuere. Praesent congue sem eros, sit amet feugiat lectus blandit in. Mauris sed placerat tortor. Quisque porta sed est quis porta. Aliquam erat volutpat. Integer fermentum tellus a lorem suscipit blandit.'});
+    this.notifylist.push({id: 2, assunto: 'teste 2', corpo: 'mensagem'});
+    this.notifylist.push({id: 3, assunto: 'teste 3', corpo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis tincidunt leo, nec semper est tempus sit amet. Suspendisse eu ligula eu tellus convallis auctor. Aenean ac pellentesque tellus, at vehicula odio. Pellentesque euismod lacinia libero vitae vehicula. Sed nec ullamcorper quam. Curabitur aliquam interdum posuere. Praesent congue sem eros, sit amet feugiat lectus blandit in. Mauris sed placerat tortor. Quisque porta sed est quis porta. Aliquam erat volutpat. Integer fermentum tellus a lorem suscipit blandit.'});
+    this.notifylist.push({id: 4, assunto: 'teste 4', corpo: 'mensagem'});
+    this.notifylist.push({id: 5, assunto: 'teste 5', corpo: 'mensagem'});
+    this.auth.emitisLoggin.subscribe(ok => this.useractive = ok);
+
   }
 
   onBtnSlideClick() {
