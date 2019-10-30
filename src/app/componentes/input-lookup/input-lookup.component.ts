a2ed9bdce297db00378b74d7022fc573ed8f6108
MARCELO HORLLE <joehor@gmail.com>
2019-10-30 10:07:44 -0300

    Abre janela lookup do grid

--- src/app/componentes/input-lookup/input-lookup.component.ts
+++ src/app/componentes/input-lookup/input-lookup.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-input-lookup',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./input-lookup.component.css']
 })
 export class InputLookupComponent implements OnInit {
-
-  constructor() { }
+  modalRef: BsModalRef;
+  inputid: number;
+  inputname: string;
+  selected: any;
+  constructor(private modalService: BsModalService) { }
 
   ngOnInit() {
   }
 
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
+
+  onSelectData(event: any) {
+
+    this.selected = event;
+    console.log('representantes-lookup(onSelectData): ' + JSON.stringify(event));
+
+  }
+
+  confirm() {
+    console.log('Selecionado o item: ' + JSON.stringify(this.selected));
+    this.inputid = this.selected['id'];
+    this.inputname = this.selected['nome'];
+    this.modalRef.hide();
+  }
 }
