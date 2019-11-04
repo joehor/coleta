177d91bd258930ab44fca3853c81dc05d4974805
MARCELO HORLLE <horlle@kappesberg.com.br>
2019-11-04 01:49:00 -0300

    testes com typeahead

--- src/app/componentes/input-lookup/input-lookup.component.ts
+++ src/app/componentes/input-lookup/input-lookup.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, TemplateRef, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { HttpClient } from '@angular/common/http';
+
+import { debounceTime, tap, switchMap, finalize } from 'rxjs/operators';
 import { DataLookupService } from '../../services/data-lookup.service';
+import { DataApi } from '../../services/data-api';
+import { TabHeadingDirective } from 'ngx-bootstrap';
 
 interface Httpcodes {
   code: number;
@@ -26,16 +32,68 @@ export class InputLookupComponent implements OnInit, OnChanges {
   @Input() title = 'Não Informado';
   @Input() pagesize = 10;
   @Input() lookupselected: Lookup = {id: 0, descricao: ''};
+  apidata: any[] = [];
+  searchDataCtrl = new FormControl();
+  waitsearch: any;
 
   modalRef: BsModalRef;
   apierror = false;
   pesquisando = false;
-  httperror: Httpcodes;
+  httperror: Httpcodes = { code: 0, erro: '', mensagem: ''};
 
-  constructor(private modalService: BsModalService, private datalookup: DataLookupService) { }
+  constructor(private modalService: BsModalService, private datalookup: DataLookupService, private http: HttpClient) { }
 
   ngOnInit() {
-  }
+
+    // busca todos os eventos ...
+    this.getDataFromApi(this.apiroute, '%', 1, 1000);
+
+    this.apidata = JSON.parse(localStorage.getItem('spr_repres'));
+    console.log(this.apidata);
+/*
+    this.searchDataCtrl.valueChanges
+      .pipe(
+        debounceTime(500),
+        tap(() => {
+          this.httperror.mensagem = '';
+          this.apidata = [];
+          this.pesquisando = true;
+        }),
+        switchMap(value => {
+            const params = {
+              pesquisa: value,
+              PageNumber: '1',
+              PageSize: '1000'
+            };
+            return this.http.get<DataApi>( 'api/' + this.apiroute, { params } )
+          .pipe(
+            finalize(() => {
+              this.pesquisando = false;
+            }),
+          );
+        })
+      )
+      .subscribe(data => {
+        if (data.Data === undefined) {
+          this.httperror.mensagem = 'erro ao pesquisar'; // data['Error'];
+          this.apidata = [];
+        } // else {
+          // if (this.inputname !== data.Data[0].descricao) {
+          // this.httperror.mensagem = '';
+          // this.apidata = data.Data; //.map(list => list.descricao);
+          // if (this.apidata.length === 1) {
+          //  this.inputid = this.apidata[0].id;
+          //  this.inputname = this.apidata[0].descricao;
+          // }
+        // }
+        }
+
+        console.log('apidata: ' + JSON.stringify(this.apidata));
+      });
+*/
+
+    }
+
 
   ngOnChanges(changes: SimpleChanges) {
     console.log('mudou algo');
@@ -81,6 +139,19 @@ export class InputLookupComponent implements OnInit, OnChanges {
     }
   }
 
+  onKeySearch() {
+    clearTimeout(this.waitsearch);
+    this.waitsearch = setTimeout(() => {
+      this.getDataByName(this.inputname);
+    }, 1000);
+  }
+
+  getDataByName(search: any) {
+
+    this.getDataFromApi(this.apiroute, search, 1, 1000);
+
+  }
+
   // serviço que busca os dados da API...
   getDataFromApi(api: string, pesq: string, page: number, pagecount: number) {
 
@@ -92,17 +163,18 @@ export class InputLookupComponent implements OnInit, OnChanges {
           console.log('Não encontrado!');
           this.pesquisando = false;
           this.inputid = 0;
+          this.apidata = [];
         } else {
           console.log('Encontrado');
           this.pesquisando = false;
           this.apierror = false;
           this.httperror = null;
+          this.apidata = data.Data;
           // this.lookupselected = {id: data.Data[Object.keys(data.Data)[0]], descricao: data.Data[Object.keys(data.Data)[1]]};
           data.Data.map((row: Lookup) => {
             this.lookupselected = {id: row.id, descricao: row.descricao};
           });
-          this.inputid = this.lookupselected.id;
-          this.inputname = this.lookupselected.descricao;
+          this.datalistSelected();
           // this.confirm();
         }
       },
@@ -114,5 +186,16 @@ export class InputLookupComponent implements OnInit, OnChanges {
         if (error.code === 401) { this.httperror.mensagem = 'Falha na autenticação, efetue novo logon'; }
       }
     );
+  }
+
+  datalistSelected() {
+    this.inputid = this.lookupselected.id;
+    this.inputname = this.lookupselected.descricao;
+  }
+
+  lista() {
+
+    return this.apidata.map(desc => desc.descricao);
+
   }
 }
