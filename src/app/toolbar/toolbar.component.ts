0aac7364a807e9a4c4ea5af48333a93b99231317
MARCELO HORLLE <horlle@kappesberg.com.br>
2019-11-06 00:44:57 -0300

    ajuste abertura do sidemenu

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -26,9 +26,6 @@ export class ToolbarComponent implements OnInit {
 
   ngOnInit() {
 
-
-
-
     this.notifylist.push({id: 1, assunto: 'teste 1', corpo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis tincidunt leo, nec semper est tempus sit amet. Suspendisse eu ligula eu tellus convallis auctor. Aenean ac pellentesque tellus, at vehicula odio. Pellentesque euismod lacinia libero vitae vehicula. Sed nec ullamcorper quam. Curabitur aliquam interdum posuere. Praesent congue sem eros, sit amet feugiat lectus blandit in. Mauris sed placerat tortor. Quisque porta sed est quis porta. Aliquam erat volutpat. Integer fermentum tellus a lorem suscipit blandit.'});
     this.notifylist.push({id: 2, assunto: 'teste 2', corpo: 'mensagem'});
     this.notifylist.push({id: 3, assunto: 'teste 3', corpo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas venenatis tincidunt leo, nec semper est tempus sit amet. Suspendisse eu ligula eu tellus convallis auctor. Aenean ac pellentesque tellus, at vehicula odio. Pellentesque euismod lacinia libero vitae vehicula. Sed nec ullamcorper quam. Curabitur aliquam interdum posuere. Praesent congue sem eros, sit amet feugiat lectus blandit in. Mauris sed placerat tortor. Quisque porta sed est quis porta. Aliquam erat volutpat. Integer fermentum tellus a lorem suscipit blandit.'});
@@ -39,15 +36,12 @@ export class ToolbarComponent implements OnInit {
   }
 
   onBtnSlideClick() {
-
-    console.log('onBtnSlideClick...');
-    // this.sidmenu = !this.sidmenu;
-    // this.emitSidemenu.emit(this.sidmenu);
-    if (document.documentElement.style.getPropertyValue('--main-slidemenu-width') === '0') {
-      document.documentElement.style.setProperty('--main-slidemenu-width', '-350px');
-    } else {
+    if (document.documentElement.style.getPropertyValue('--main-slidemenu-width') !== '0') {
       document.documentElement.style.setProperty('--main-slidemenu-width', '0');
+    } else {
+      document.documentElement.style.setProperty('--main-slidemenu-width', '-350px');
     }
+
   }
 
   // tslint:disable-next-line: use-lifecycle-interface
