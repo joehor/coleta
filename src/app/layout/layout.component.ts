7b591d02a3b68c529b214bf51ecfe4883488d8d8
MARCELO HORLLE <joehor@gmail.com>
2020-01-21 15:29:44 -0300

    Adicionado chamadas e exibição de avisos por aqui

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataLookupService } from '../services/data-lookup.service';
 
 @Component({
   selector: 'app-layout',
@@ -8,14 +9,33 @@ import { Component, OnInit } from '@angular/core';
 export class LayoutComponent implements OnInit {
 
   sidemenuopen = false;
+  error: any;
+  success: any;
+  loading: boolean;
+  loadmessage: boolean;
 
-  constructor() { }
+  constructor(private datalookup: DataLookupService) {
+    this.datalookup.emitUpdateStatus
+    .subscribe(data => {
+      this.loading = !data.complete;
+      this.loadmessage = data.mensagem;
+    } );
+  }
+
   ngOnInit() {
   }
 
   slide(event) {
     this.sidemenuopen = event;
     console.log('event: ' + event);
+  }
+
+  showError(msg: string) {
+    this.error = {mensagem: msg};
+  }
+
+  showSuccess(msg: string) {
+    this.success = {mensagem: msg};
   }
 
 }
