67bb11868844697eabf51c6dbf84e1f724c6ecc0
MARCELO HORLLE <horlle@kappesberg.com.br>
2020-01-22 23:53:12 -0300

    autenticado vai para dashboard

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -40,7 +40,7 @@ export class LoginComponent implements OnInit, AfterViewInit {
 
         if (this.authservice.isAuthenticated()) {
 
-          this.route.navigate(['/home']);
+          this.route.navigate(['/dashboard']);
 
         } else {
 
