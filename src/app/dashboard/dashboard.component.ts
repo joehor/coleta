0d55c1fdc7b6e0e4aca76c20d560da48104fc212
MARCELO HORLLE <joehor@gmail.com>
2020-01-23 14:22:22 -0300

    Ajustes formatação

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+
+    // queria aplicar o fundo somente depois de terminar de renderizar os componentes ...
+    // document.documentElement.style.setProperty('--OffLineBackground', 'green');
+
   }
 
 }
