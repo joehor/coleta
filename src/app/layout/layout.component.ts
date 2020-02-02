4c22dea63401f88aad5a80e2cf34f02e87d33994
MARCELO HORLLE <horlle@kappesberg.com.br>
2020-02-02 00:19:35 -0300

    ajuste tipo de dados dos alertas

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataLookupService } from '../services/data-lookup.service';
+import { DataPostService } from '../services/data-post.service';
 
 @Component({
   selector: 'app-layout',
@@ -9,16 +10,23 @@ import { DataLookupService } from '../services/data-lookup.service';
 export class LayoutComponent implements OnInit {
 
   sidemenuopen = false;
-  error: any;
-  success: any;
-  loading: boolean;
-  loadmessage: boolean;
+  error: any = {error: false};
+  success: any = {succes: false};
+  loading: any = {loading: false};
 
-  constructor(private datalookup: DataLookupService) {
+  constructor(private datalookup: DataLookupService, private datapost: DataPostService) {
     this.datalookup.emitUpdateStatus
     .subscribe(data => {
-      this.loading = !data.complete;
-      this.loadmessage = data.mensagem;
+      this.loading = {loading: !data.complete, message: data.mensagem};
+    } );
+
+    this.datapost.emitDataPost
+    .subscribe(data => {
+      if (data.error) {
+       this.showError(data.mensagem);
+      } else {
+        this.showSuccess(data.mensagem);
+      }
     } );
   }
 
@@ -31,11 +39,15 @@ export class LayoutComponent implements OnInit {
   }
 
   showError(msg: string) {
-    this.error = {mensagem: msg};
+    this.error = {error: true, mensagem: msg};
   }
 
   showSuccess(msg: string) {
-    this.success = {mensagem: msg};
+    this.success = {success: true, mensagem: msg};
+  }
+
+  showLoading(msg: string) {
+    this.loading = {loading: true, message: msg};
   }
 
 }
